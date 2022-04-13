2f05d0d5356cac6041ff4c55c0788b7a17b7c83a
Ken Fehling <me@kenfehling.com>
2022-04-13 15:04:31 -0400

    astroturf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,21 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import { babel } from '@rollup/plugin-babel';
+// import postcss from 'rollup-plugin-postcss';
 
 // https://astro.build/config
 export default defineConfig({
-	// Enable React to support React JSX components.
 	integrations: [react()],
+	vite: {
+		plugins: [
+			babel({
+			  plugins: ['astroturf/plugin'],
+			})
+			// postcss({
+			//   extract: 'app.css',
+			//   modules: true,
+			// //   plugins: [postcssNested],
+			// }),
+		  ]
+	}
 });
--- src/components/Cat.jsx
+++ src/components/Cat.jsx
@@ -1,12 +1,48 @@
-import React from 'react';
-import './Cat.css';
+import React from 'react'
+// import styled from 'styled-components'
+// import {css} from '@emotion/react'
+// import styled from '@emotion/styled'
+import { css } from "astroturf";
+
+// console.log("AMN");
+// console.log(styled.div);
+
+// const CatContainer = styled.div`
+// 	opacity: 0.5;
+// 	width: 50%;
+// 	border-radius: 4px;
+// 	&:hover {
+// 		opacity: 1;
+// 	}
+// 	& > img {
+// 		width: 100%;
+// 	}
+// `;
+
+// const Cat = ({image, name, description}) => (
+// 	<CatContainer>
+// 		<div>{name}</div>
+// 		<img src={image} alt={name} />
+// 		<div>{description}</div>
+// 	</CatContainer>
+// );
 
 const Cat = ({image, name, description}) => (
-		<div>
-				<div>{name}</div>
-				<img className='cat-image' src={image} alt={name} />
-				<div>{description}</div>
-		</div>
+	<div className={css`
+		opacity: 0.5;
+		width: 50%;
+		border-radius: 4px;
+		&:hover {
+			opacity: 1;
+		}
+		& > img {
+			width: 100%;
+		}
+	`}>
+		<div>{name}</div>
+ 		<img src={image} alt={name} />
+ 		<div>{description}</div>
+	</div>
 );
 
 export default Cat
