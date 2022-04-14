760bcf302d12efb40cba0eb5477c10868453f754
Ken Fehling <me@kenfehling.com>
2022-04-13 21:44:53 -0400

    Linaria working well

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,6 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import linaria from '@linaria/rollup';
 
-console.log(linaria.default);
-
-// https://astro.build/config
 export default defineConfig({
 	integrations: [react()],
 	vite: {
--- src/components/Cat.jsx
+++ src/components/Cat.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import { css } from '@linaria/core';
+import { styled } from '@linaria/react';
+
+const CatContainer = styled.div`
+	opacity: 0.5;
+	width: 50%;
+	border-radius: 4px;
+	&:hover {
+		opacity: 1;
+	}
+	& > img {
+		width: 100%;
+	}
+`;
 
 const Cat = ({image, name, description}) => (
-	<div className={css`
-		opacity: 0.5;
-		width: 50%;
-		border-radius: 4px;
-		&:hover {
-			opacity: 1;
-		}
-		& > img {
-			width: 100%;
-		}
-	`}>
+	<CatContainer>
 		<div>{name}</div>
  		<img src={image} alt={name} />
  		<div>{description}</div>
-	</div>
+	</CatContainer>
 );
 
 export default Cat
